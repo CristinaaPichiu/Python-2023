d4c311fcba5b9bd1a2a0a600e59c80a33ead1a6c
Pichiu Cristina-Cătălina <118998541+CristinaaPichiu@users.noreply.github.com>
2023-10-16 12:11:20 +0300

    Delete Laborator1 directory

--- Laborator1/exercise10.py
+++
@@ -1,7 +0,0 @@
-def count_words(text):
-    words = text.split()
-    return len(words)
-
-text = "I have Python exam"
-count = count_words(text)
-print(f"Numarul de cuvinte din text este: {count}")
