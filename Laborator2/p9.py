d993b84c9bea440cf162a733126dcbafc9712ad6
Cristina Pichiu <cristinapichiu38@gmail.com>
2023-10-23 00:21:35 +0300

    p9 lab2

--- Laborator2/p9.py
+++ Laborator2/p9.py
@@ -1,26 +1,39 @@
-def verificare_ordonare_coloane(matrice):
-    n = len(matrice)
-    m = len(matrice[0])
+def coloane_neordonate_descrescator(matrice):
+    index_coloane_neordonate = []
 
-    elemente_neordonate = []
+    for coloana in range(len(matrice[0])):
+        este_descrescator = True
+        for rand in range(1, len(matrice)):
+            if matrice[rand][coloana] <=  matrice[rand - 1][coloana]:
+                este_descrescator = False
+                break
+        if not este_descrescator:
+            index_coloane_neordonate.append(coloana)
+    return index_coloane_neordonate
 
-    for coloana in range(m):
-        for rand in range(1, n):
-            if matrice[rand][coloana] <= matrice[rand - 1][coloana]:
-                elemente_neordonate.append((rand, coloana))
+def afiseaza_pozitii(matrice, coloane_neordonate):
+    for coloana in coloane_neordonate:
+        elemente_afisate = set()
+        for rand in range(1, len(matrice)):
+            element_curent = matrice[rand][coloana]
+            for linie_deasupra in range(rand):
+                if matrice[linie_deasupra][coloana] >= element_curent:
+                    pozitie = (rand, coloana)
+                    if pozitie not in elemente_afisate:
+                        print(f"  ({rand}, {coloana})")
+                        elemente_afisate.add(pozitie)
 
-    if elemente_neordonate:
-        for pozitie in elemente_neordonate:
-            print(f"({pozitie[0]}, {pozitie[1]})")
-    else:
-        print("Toate coloanele sunt ordonate crescător de sus în jos.")
 
 # Exemplu de utilizare:
-matrice = [
-    [1, 2, 3, 2, 1, 1],
-    [2, 4, 4, 3, 7, 2],
-    [5, 5, 2, 5, 6, 4],
-    [6, 6, 7, 6, 7, 5]
-]
+matrice = [[1, 2, 3, 2, 1, 1],
 
-verificare_ordonare_coloane(matrice)
+[2, 4, 4, 3, 7, 2],
+
+[5, 5, 2, 5, 6, 4],
+
+[6, 6, 7, 6, 7, 5]]
+
+indexe = coloane_neordonate_descrescator(matrice)
+print("Coloanele care nu sunt ordonate descrescător sunt:", indexe)
+afiseaza_pozitii(matrice, indexe)
+
