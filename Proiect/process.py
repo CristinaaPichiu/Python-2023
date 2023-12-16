3bf82e14dc6f74df094404e5f5d0e31d78e0d769
Cristina Pichiu <cristinapichiu38@gmail.com>
2023-12-16 12:55:13 +0200

    suspend + resume

--- Proiect/process.py
+++ Proiect/process.py
@@ -3,39 +3,36 @@ import subprocess
 import sys
 import psutil
 
+
 def help_specific_command(command):
     if command == "view":
-        print("view: Afișează informații despre toate procesele în execuție.")
+        print("view: displays information about all running processes.")
     elif command == "suspend":
-        print("suspend <PID>: Suspendă execuția procesului cu PID-ul specificat.")
+        print("suspend <PID>: suspends execution of the process with the specified PID.")
     elif command == "resume":
-        print("resume <PID>: Reia execuția procesului cu PID-ul specificat.")
+        print("resume <PID>: resumes execution of the process with the specified PID.")
     elif command == "kill":
-        print("kill <PID>: Termină procesul cu PID-ul specificat.")
+        print("kill <PID>: stops the process with the specified PID.")
     elif command == "run":
-        print("run <path> <arguments>: Rulează un nou proces cu path-ul si argumentele specificate.")
+        print("run <path> <arguments>: runs a new process with the specified path and arguments.")
     else:
-        print(f"Comanda '{command}' nu exista.")
+        print(f"Command: '{command}' doesnt exists.")
 
 
 def exists_command(command):
     try:
         list_commands = ["view", "suspend", "resume", "kill", "run", "help", "exit"]
         if command not in list_commands:
-            raise ValueError("Comanda introdusă nu este validă.")
-        return True  # Comanda există în listă
-    except ValueError as e:
-        print(f"Error: {e}")
-        return False  # Comanda nu există în listă
+            raise ValueError("The command entered is not valid.")
+        return True
     except Exception as e:
         print(f"Error: {e}")
-        return False  # În caz de altă excepție, comanda nu există în listă
-
+        return False
 
 
 def is_syntax_correct(command, args):
     try:
-        if command == "view" and len(args)==0:
+        if command == "view" and len(args) == 0:
             return True
         elif command == "help" and len(args) == 1:
             return True
@@ -46,10 +43,7 @@ def is_syntax_correct(command, args):
         elif command == "exit" and len(args) == 0:
             return True
         else:
-            raise ValueError("Sintaxă incorectă pentru comanda specificată.")
-    except ValueError as e:
-        print(f"Error: {e}")
-        return False
+            raise ValueError("Incorrect syntax for the specified command.")
     except Exception as e:
         print(f"Error: {e}")
         return False
@@ -60,19 +54,20 @@ def view_command():
         try:
             pid = process.pid
             if pid != 0 and psutil.pid_exists(pid):
-                print("------------------------------------------------------------------------------------" )
+                print("------------------------------------------------------------------------------------")
                 print(f'Process Info for PID {pid}: ')
                 print(f'  PID: {pid}')
                 print(f'  Name: {process.name()}')
+                print(f'  Path: {process.exe()}')
                 print(f'  CPU Percent: {process.cpu_percent()}')
-                list= process.memory_info()
-                print(f'  Memory Info: {list} ')
+                variable = process.memory_info()
+                print(f'  Memory Info: rss: {variable.rss}, vms: {variable.vms} ')
                 print(f'  Create Time: {process.create_time()}')
                 print(f'  Status: {process.status()}')
                 print(f'  Username: {process.username()}')
                 print(f'  Connections: {process.connections()}')
                 print(f'  Number of Threads: {process.num_threads()}')
-                print("-" * 60)
+                print("------------------------------------------------------------------------------------")
         except psutil.NoSuchProcess as e:
             print(f"Error: Process with PID {pid} does not exist. {e}")
         except psutil.AccessDenied as e:
@@ -85,8 +80,10 @@ def run_command(path, parameters):
     try:
         subprocess.run([path] + parameters)
         print("Process started succesfully. ")
+
     except Exception as e:
-     print(f"Error: {e}")
+        print(f"Error: {e}")
+
 
 def kill_command(pid):
     try:
@@ -94,10 +91,27 @@ def kill_command(pid):
         process.terminate()
         print("Process killed succesfully. ")
     except Exception as e:
-     print(f"Error: {e}")
+        print(f"Error: {e}")
+
+def suspend_command(pid):
+    try:
+        process = psutil.Process(pid)
+        process.suspend()
+        print(f"Process with pid {pid} is suspended. ")
+    except Exception as e:
+        print(f"Error: {e}")
+
+def resume_command(pid):
+    try:
+        process = psutil.Process(pid)
+        process.resume()
+        print(f"Process with pid {pid} has been resumed. ")
+    except Exception as e:
+        print(f"Error: {e}")
 
 
 if __name__ == "__main__":
+
     print("Comenzile disponibile sunt:")
     print("1. view")
     print("2. suspend <PID>")
@@ -108,14 +122,14 @@ if __name__ == "__main__":
     print("7. exit")
 
     while True:
-        user_input = input("Introdu o comandă sau 'exit' pentru a ieși: ")
+        user_input = input("Enter a command: ")
         if user_input.lower() == 'exit':
             break
 
         # impart comanda in cuvinte
         parts = user_input.split()
 
-        print (len(parts))
+        #print(len(parts))
         # primul cuvant reprezinta comanda propriu-zisa
         command = parts[0]
         # celelalte cuvinte sunt argumentele comenzii
@@ -126,7 +140,7 @@ if __name__ == "__main__":
             if command == "help" and argumente:
                 help_specific_command(argumente[0])
             if command == "view":
-                 view_command()
+                view_command()
 
             if command == "run":
                 run_command(parts[1], parts[2:])
@@ -134,7 +148,8 @@ if __name__ == "__main__":
             if command == "kill":
                 kill_command(int(parts[1]))
 
+            if command == "suspend":
+                suspend_command(int(parts[1]))
 
-
-
-
+            if command == "resume":
+                resume_command(int(parts[1]))
